6b4c77f7147847a3cdbfbae73a3463ba337d555c
Gisela <giselacarballour@gmail.com>
2025-03-20 13:52:31 +0100

    objetos

--- JS/Ejercicio3-Bucles/bucles.js
+++ JS/Ejercicio3-Bucles/bucles.js
@@ -5,3 +5,39 @@ for (i=2; i<20; i++){
 }
 
 //while
+let a=0;
+while(a<6){
+ console.log('hola, soy el número '+a)
+  a++;
+}
+
+// Array
+
+let frutas=['Banana','manzana','mandarina', 'pera']
+
+// for(i=0; i<frutas.length; i++){
+//   if(frutas[i].length<10){
+//     console.log(frutas[i]);
+//   }
+// }
+
+for(let fruta of frutas){
+  console.log(fruta)
+}
+
+frutas.push('melón', 'sandia')
+
+console.log(frutas)
+
+console.log(frutas.indexOf('mandarina'))
+
+
+let objetos=['pelota', 'mesa', 'botella', 'PC', 'jersei','móvil', 'carta', 'mochila', 'pizarra', 'moto',]
+
+console.log(objetos)
+
+console.log(objetos.indexOf('pizarra'))
+console.log(objetos.shift(1))
+console.log(objetos.unshift('ordenador', 'chocolate'))
+
+console.log(objetos)
---
+++ JS/Ejercicio4-Objeto/objeto.js
@@ -0,0 +1,15 @@
+const objeto={
+  nombre:'Marta',
+  edad:40,
+  Provincia:{
+    ciudad:'barcelona'
+  }, 
+  Aficiones:{
+    bailar:'Martes y Jueves',
+    teatro:'lunes'
+  }
+}
+
+console.log(objeto)
+
+console.log(objeto.Aficiones)
