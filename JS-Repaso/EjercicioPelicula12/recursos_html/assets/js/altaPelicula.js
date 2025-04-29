dc50851bc4921d341ead4248f61d91b722f1ef0a
Gisela <giselacarballour@gmail.com>
2025-04-29 14:18:11 +0200

    ejercicioPelicula

--- JS-Repaso/EjercicioPelicula12/recursos_html/assets/js/altaPelicula.js
+++ JS-Repaso/EjercicioPelicula12/recursos_html/assets/js/altaPelicula.js
@@ -1,27 +1,41 @@
 function alta(){
   consultaCategorias()
 
-  document.querySelector("#altapelicula").onclick = altaPelicula
+  document.querySelector("#formalta").onsubmit= function(ev){
+    ev.preventDefault()
+    altaPelicula()
+  }
+    
   document.querySelector('#portada').oninput = previsualizarImagen // cuando cambia el valor de ese campo
 }
 function altaPelicula(){
+  // recuperar las cajas de avisos y errores
+  const avisoOK = document.querySelector('.alert-success')
+  const avisoNOK = document.querySelector('.alert-danger')
   
+  //ocultar las cajas de avisos y errores
+  avisoOk.classList.add('avisos')
+  avisoNOK.classList.add('avisos')
+  
+
   const API_Alta= 'https://movies.alcyon-it.com/api/peliculas/'
-  
+  //recuperar los datos del formularo
   let titulo = document.querySelector('#titulo').value.trim()
   let categoria = document.querySelector('#idcategoria').value.trim()
   let direccion= document.querySelector('#direccion').value.trim()
   let anio = document.querySelector('#anio').value
-  let sipnosis = document.querySelector('#sinopsis').value.trim()
-  let portada = document.querySelector('#portada').files[0]
+  let sinopsis = document.querySelector('#sinopsis').value.trim()
+  let imagen = document.querySelector('#portada').files[0]
+  // validar datos
 
+  //confeccionar el objeto formData
   const formdata= new FormData()
   formdata.append('titulo', titulo)
   formdata.append('direccion', direccion)
   formdata.append('anio', anio)
   formdata.append('idcategoria', categoria)
-  formdata.append('sipnosis', sipnosis)
-  if(portada)formdata.append('img', portada)
+  formdata.append('sinopsis', sinopsis)
+  if(imagen)formdata.append('img', imagen)
   
     const parametro ={
       'method':'POST',
@@ -36,10 +50,20 @@ function altaPelicula(){
         throw(respuesta.json())
       }
     })
-    .then(mensaje => console.log(mensaje))
-    .catch(error => error.then(er=> console.log(er))) 
+    .then(mensaje => {console.log(mensaje)
+      avisoOK.classList.remove('avisos')}
+  )
+    .catch(error => error.then(er => {
+    console.log(er)
+    let errores = ''
+    er.forEach(e => {
+      errores += `<div>${e}</div>`
+    })
+    avisoNOK.innerHTML = errores // mostrar los errores en la caja
+    avisoOK.classList.remove('avisos') //visualizar la caja
+  })) 
     
-    previsualizar()
+    previsualizarImagen()
 
 }
 
