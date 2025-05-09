7d1424d3b8eab032e9368fa4527cec8c27ee889a
Gisela <giselacarballour@gmail.com>
2025-05-09 09:03:04 +0200

    Proyecto Estados

---
+++ REACT/estados/assents/header.jsx
@@ -0,0 +1,19 @@
+import 'header'
+function header() {
+  return (
+    <>
+    <div>
+      <nav>
+        <ol>
+          <li>Inicio</li>
+          <li>Sobre Nosotros</li>
+          <li>Contacto</li>
+        </ol>
+      </nav>
+    </div>
+
+    </>
+  );
+}
+
+export default header;
---
+++ REACT/estados/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <header> </header>
+    <h1>Estados</h1>
+
+    </>
+  );
+}
+
+export default App;
