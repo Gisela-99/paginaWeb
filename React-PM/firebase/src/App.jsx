eede2493fe7c1fb54f2a3ea60cb9576d3663c2d5
Gisela <giselacarballour@gmail.com>
2025-06-02 10:50:07 +0200

    Para leer el firebase

--- React-PM/firebase/src/App.jsx
+++ React-PM/firebase/src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from "react"
-import {createPerson} from './services/api';
+import { useState,useEffect } from "react"
+import {createPerson,getItems} from './services/api';
 
 function App() {
   const [name,setName]=useState();
+  const [items, setItems] = useState([]); 
 
   const handleCreate =()=>{
   createPerson({name})
   }
   
+   useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const data = await getItems();
+        setItems(data); // Guardamos los ítems en el estado
+      } catch (error) {
+        console.error("Error al obtener los ítems:", error);
+      }
+    };
+
+    fetchItems();
+  }, []);
   return (
     <>
     <input type="text"  onChange={(e) => setName(e.target.value)}/>
-      <button onClick={handleCreate}>Create</button>
+    <button onClick={handleCreate}>Create</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
