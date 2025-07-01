f511c6aaf5254fc9c4b47586489b66039c9694b4
Gisela <giselacarballour@gmail.com>
2025-07-01 10:31:45 +0200

    Ejercicio rutas get

--- NODE/index.js
+++ NODE/index.js
@@ -7,9 +7,19 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("Success!");
+  res.json("Inicio");
 });
 
+app.get('/libro', (req, res)=>{
+  console.log("Llegamos a libro");
+  res.send("Llegamos a libro");
+})
+
+app.get('/book/:id', (req, res) => {
+    const bookId = req.params.id; // Captura la id del libro
+    res.send(`Id del libro ${bookId}`);
+});
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
+  console.log(`hudfiwerñfhrewiu`)
 });
